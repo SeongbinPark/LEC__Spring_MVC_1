6e0d5a78513a3d95568243560566e41c2ad1704b
SeongbinPark <binni0515@gmail.com>
2022-08-26 21:52:42 +0900

    ResponseBodyController : HTTP API를 제공하는 경우 응답 메세지로 HTML이 아니라 데이터를 전달해야 한다. 이때 HTTP 메세지 바디에는 JSON같은 형식으로 데이터를 실어서 보낸다.

---
+++ src/main/java/hello/springmvc/response/ResponseBodyController.java
@@ -0,0 +1,61 @@
+package hello.springmvc.response;
+
+import hello.springmvc.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+// HTTP API를 제공하는 경우 응답 메세지로 HTML이 아니라 데이터를 전달해야 한다.
+// 이때 HTTP 메세지 바디에는 JSON같은 형식으로 데이터를 실어서 보낸다.
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @GetMapping("/response-body-string-v3")
+    @ResponseBody
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("catsbi");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("catsbi");
+        helloData.setAge(20);
+
+        return helloData;
+    }
+
+
+}
